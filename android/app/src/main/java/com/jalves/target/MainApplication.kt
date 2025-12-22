e6f0a12e91550df78c4f198b929d8a2bffff92d4
Jonathan Alves <jonathansilvaalves16@gmail.com>
2025-12-22 08:47:51 -0300

    Added expo router and icons for app

--- android/app/src/main/java/com/jalves/target/MainApplication.kt
+++ android/app/src/main/java/com/jalves/target/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.target
+package com.jalves.target
 
 import android.app.Application
 import android.content.res.Configuration
---
+++ src/app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from 'react-native';
+
+export default function Index() {
+    return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Open up src/app/index.tsx to start working on your app!</Text>
+    </View>
+    );
+}
